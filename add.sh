ecc6636cef8a47ed713d777efec87c5b7facbb6d
Lukáš Drahník <ldrahnik@gmail.com>
2023-04-16 23:11:52 +0000

    Delete add.sh

--- add.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-cat new.log | grep '^popup.js:*' | cut -b 20- | sort -n -t"." -k3 -k2 -k1 | uniq | awk NF >> report/report && cat report/report | sort -n -t"." -k3 -k2 -k1 | uniq | awk NF
